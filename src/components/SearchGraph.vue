7d323907dd0f1231206543c70ff759d19d089e97
Qztyv <nickocowling@gmail.com>
2021-02-15 16:30:20 +0000

    refactored searchpage

--- src/components/SearchGraph.vue
+++ src/components/SearchGraph.vue
@@ -6,7 +6,8 @@
         class="center"
         v-for="graph in graphs"
         :key="graph.graphId"
-        :graph="graph"
+        :graphInformation="graph.graphInformation"
+        :yPlots="graph.yPlots"
       ></line-chart>
     </Suspense>
   </div>
@@ -14,7 +15,7 @@
 
 <script>
 import LineChart from "@/components/LineChart";
-import { graphsCollection } from "@/firebase/config";
+import { getGraphsBySearchTerm } from "@/firebaseFunctions/getGraph";
 import { ref } from "vue";
 
 export default {
@@ -34,29 +35,11 @@ export default {
     }
   },
   async setup(props) {
-    const graphs = ref(await getGraphs());
+    const graphs = ref(await getGraphsBySearchTerm("userId", props.userId));
     const error = ref("");
 
-    async function getGraphs() {
-      let graphCollection = [];
-
-      await graphsCollection
-        .where("userId", "==", props.userId)
-        // .where("cardioMyopathyType", "==", props.cardioMyopathyType)
-        // .where("mutatedGeneType", "==", props.mutatedGeneType)
-        .get()
-        .then(querySnapshot => {
-          querySnapshot.forEach(doc => {
-            let graph = {
-              graphId: doc.id,
-              graphInformation: doc.data()
-            };
-            graphCollection.push(graph);
-          });
-        });
-      return graphCollection;
-    }
-
+    //     // .where("cardioMyopathyType", "==", props.cardioMyopathyType)
+    //     // .where("mutatedGeneType", "==", props.mutatedGeneType)
     if (graphs.value.length === 0) {
       error.value = "No graphs found matching search";
     }
