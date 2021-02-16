8c0da5de8250e86c3f1b5a39af3f91bebe7f802a
Wahab Rehman <wahab_rehman1256@outlook.com>
2021-02-16 12:33:09 +0000

    Just a extra thing i did for the search page
    Just a way to enhance the user experience. We can only search by one thing at a time, so why show two drop downs? Made a toggle btns show it shows the drop downs one at a time

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="container">
-    <section>
+  <div>
+    <section class="container">
       <div class="search">
         <h2>Search Graphs</h2>
+        <div class="search-btns">
+          <button v-bind:disabled="!hideCardioType" @click="toggleCardioType">
+            Cardiomyopathy Type
+          </button>
+          <button v-bind:disabled="!hideGeneType" @click="toggleGeneType">
+            MutatedGene Type
+          </button>
+        </div>
         <form @submit.prevent="submitData">
-          <BaseSelect
-            :options="cardiomyopathyTypeOptions"
-            v-model="cardiomyopathyData.cardiomyopathyType"
-            label="Select Cardiomyopathy Type"
-          />
-          <br />
-          <BaseSelect
-            :options="mutatedGeneTypeOptions"
-            v-model="cardiomyopathyData.mutatedGeneType"
-            label="Select Mutated Gene Type"
-          />
+          <div v-if="!hideCardioType">
+            <BaseSelect
+              :options="cardiomyopathyTypeOptions"
+              v-model="cardiomyopathyData.cardiomyopathyType"
+              label="Select Cardiomyopathy Type"
+            />
+          </div>
+
+          <div v-if="!hideGeneType">
+            <BaseSelect
+              :options="mutatedGeneTypeOptions"
+              v-model="cardiomyopathyData.mutatedGeneType"
+              label="Select Mutated Gene Type"
+            />
+          </div>
           <br />
 
           <input type="submit" />
@@ -43,6 +55,19 @@ export default {
     BaseSelect
   },
   setup() {
+    const hideCardioType = ref(false);
+    const hideGeneType = ref(true);
+
+    const toggleCardioType = () => {
+      hideCardioType.value = !hideCardioType.value;
+      hideGeneType.value = true;
+    };
+
+    const toggleGeneType = () => {
+      hideGeneType.value = !hideGeneType.value;
+      hideCardioType.value = true;
+    };
+
     const { user } = getUser();
 
     const { userDetails, error } = getUserDetails(`${user.value.uid}`);
@@ -75,23 +100,44 @@ export default {
       cardiomyopathyTypeOptions,
       user,
       userDetails,
-      error
+      error,
+      hideCardioType,
+      hideGeneType,
+      toggleCardioType,
+      toggleGeneType
     };
   }
 };
 </script>
 
 <style scoped>
+/*
 .search {
   width: 20%;
   margin: 5px;
   padding: 1em;
   float: left;
 }
+*/
+
+.container {
+  display: flex;
+  padding: 60px;
+  justify-content: center;
+}
+.search-btns {
+  display: flex;
+}
+
+.graphs {
+  gap: 332px;
+}
+/*
 .graphs {
   width: 60%;
   margin: 5px;
   padding: 1em;
   float: right;
 }
+*/
 </style>
