b56893066d8c18f2c76c5ab09c007d0fd07be561
Qztyv <nickocowling@gmail.com>
2021-02-18 11:12:34 +0000

    moved the async setup code from graphdetails to a new component that handles the async firebase call. That component can now be wrapped in Suspense tags so that we can attach a fallback for the loading icon. Loading icon now appears on view details

--- src/views/GraphDetails.vue
+++ src/views/GraphDetails.vue
@@ -1,51 +1,33 @@
 <template>
-  <div>
-    <h1>Graph Details</h1>
-
-    <p>{{ graphInformation.cardiomyopathyType }}</p>
-    <p>{{ graphInformation.mutatedGeneType }}</p>
-    <p>{{ graphInformation.typeOfData }}</p>
-    <p>{{ graphInformation.paper }}</p>
-    <p>{{ graphInformation.timeOfInsert }}</p>
-    <div>
-      <LineChart
-        class="center"
-        :graphInformation="graphInformation"
-        :yPlots="yPlots"
-      />
-    </div>
-  </div>
-
-  <div v-if="error" class="error">{{ error }}</div>
+  <h1>Graph Details</h1>
+  <Suspense>
+    <template #default>
+      <SingleGraph :id="id" />
+    </template>
+    <template #fallback>
+      <Loader />
+    </template>
+  </Suspense>
 </template>
 
 <script>
-import { getGraph } from "@/firebaseFunctions/getGraph";
-import LineChart from "@/components/LineChart";
-import { ref } from "vue";
+import SingleGraph from "@/components/SingleGraph.vue";
+import Loader from "../components/Loader.vue";
 export default {
   components: {
-    LineChart
+    SingleGraph,
+    Loader
   },
   props: {
     id: {
       type: String,
       required: true
     }
-  },
-  async setup(props) {
-    const { graphInformation, yPlots, error } = await getGraph(props.id);
-
-    return {
-      graphInformation: ref(graphInformation),
-      yPlots: ref(yPlots),
-      error
-    };
   }
 };
 </script>
 
-<style scoped>
+<style>
 .center {
   margin: 0 auto;
   display: flex;
