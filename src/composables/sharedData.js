464f63b4dc499c0e4726ac65f48857fafc177143
Wahab Rehman <wahab_rehman1256@outlook.com>
2021-02-23 17:46:42 +0000

    Help page

--- src/composables/sharedData.js
+++ src/composables/sharedData.js
@@ -19,4 +19,65 @@ let dataTypes = [
   "Sarcomere Shortening vs Time"
 ];
 
-export { mutatedGeneTypes, cardiomyopathyTypes, dataTypes };
+//Make sure you add id correspondingly
+
+let GuideQuestions = [
+  {
+    id: 0,
+    questions: "How do I make an account",
+    type: "para",
+
+    answer:
+      "Simply go on to the Registar page, then input all of the required details. Once you have registered you will then need to verify your account through email. Once you have verified you email you will then have access to the whole website.",
+    displayAnswer: false
+  },
+  {
+    id: 1,
+    questions: "I forgotten my password",
+    type: "para",
+    answer:
+      "No worries :) simply click on then forgotten password link. This page will ask you for your email. Once you have given you registared email, it will then send you a reset link that can be used to reset your password.",
+    displayAnswer: false
+  },
+  {
+    id: 2,
+    questions: "How do I add graph data",
+    type: "list",
+    answer: [
+      "1) Register account",
+      "2) Login",
+      "3) Click on the add graph link",
+      "4) Once you are on the add graph page, fill in the required details.",
+      "5) Input CSV file (Note the system can only take in CSV files, any other file types will not be compatitble)",
+      "6) Click Submit"
+    ],
+    displayAnswer: false
+  },
+  {
+    id: 3,
+    questions: "How do I delete my graph",
+    type: "para",
+    answer:
+      "For you to delete your graph you would need to hop on to your user dashboard. Assuming you have some graphs, there should a delete button. Click the delete button then your graph should be automatically deleted. (Note user can only delete their own graphs, a user cannot delete other users graphs)"
+  },
+  {
+    id: 4,
+    questions: "Whats the difference between the global and user dashboard",
+    type: "list",
+    answer: [
+      "UserDashbaord - The user dashboard will display your user details, as well as all the graphs you have made. ",
+      "GlobalDashbaord - The Global dashboard will display graphs made by all the users on the system."
+    ],
+    displayAnswer: false
+  },
+  {
+    id: 5,
+    questions: "I want to see a specific graph",
+    type: "para",
+    answer:
+      "Once you are logged in you can go on to the user, global and search dashboards. There each graph will have a view details button. Click the button then you will be redirected to that specific graph",
+    displayAnswer: false
+  }
+];
+
+export { mutatedGeneTypes, cardiomyopathyTypes, dataTypes, GuideQuestions };
--- src/views/Helper.vue
+++ src/views/Helper.vue
@@ -1,9 +1,41 @@
 <template>
   <h1>Got any questions?</h1>
+
+  <div v-for="question in Questions" :key="question">
+    <div @click="toggleAnswer(question.id)">
+      <h2>{{ question.questions }}</h2>
+
+      <div v-if="question.displayAnswer">
+        <div v-if="question.type == 'para'">
+          <p>{{ question.answer }}</p>
+        </div>
+        <div v-if="question.type == 'list'">
+          <ul v-for="list in question.answer" :key="list">
+            <li>{{ list }}</li>
+          </ul>
+        </div>
+      </div>
+      <div v-else>
+        click me
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { GuideQuestions } from "../composables/sharedData.js";
+import { ref } from "vue";
+export default {
+  setup() {
+    const Questions = ref();
+    Questions.value = GuideQuestions;
+
+    const toggleAnswer = id => {
+      Questions.value[id].displayAnswer = !Questions.value[id].displayAnswer;
+    };
+    return { toggleAnswer, Questions };
+  }
+};
 </script>
 
 <style></style>
